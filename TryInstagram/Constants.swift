489b6dfbe0e22bebd523709a46c81c69de7ecdff
Charles Lim <Charles@books-MacBook-Pro.local>
2017-03-08 22:02:16 +0800

    Sign in ui custom classes complete

---
+++ TryInstagram/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  TryInstagram
+//
+//  Created by Charles Lim on 3/8/17.
+//  Copyright © 2017 RegaCity. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
